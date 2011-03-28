00b2eaaeef87f138f4d273d1d67eff70da93eb96
Manuel Dejonghe <manuel@dejonghe.de>
2011-03-28 10:04:32 -0700

    add M140 as setPlatformTemperature, applies to "FiveD on RAMPS" at least. See http://reprap.org/pipermail/reprap-dev/2011-March/003113.html

--- src/replicatorg/drivers/reprap/RepRap5DDriver.java
+++ src/replicatorg/drivers/reprap/RepRap5DDriver.java
@@ -625,6 +625,12 @@ public class RepRap5DDriver extends SerialDriver {
 	public double getPlatformTemperature(){
 		return machine.currentTool().getPlatformCurrentTemperature();
 	}
+
+	public void setPlatformTemperature(double temperature) throws RetryException {
+		sendCommand(_getToolCode() + "M140 S" + df.format(temperature));
+
+		super.setPlatformTemperature(temperature);
+	}
 	/***************************************************************************
 	 * Flood Coolant interface functions
 	 **************************************************************************/
